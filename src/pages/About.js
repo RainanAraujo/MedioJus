898eb1be6d28d79736cebf2e4cf7bad83babc4a2
RainanAraujo <rainanaraaujo@gmail.com>
2020-04-19 20:54:47 -0300

    Images Render

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, StatusBar, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, StatusBar, ScrollView, Image } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import sobre from "../../assets/sobre.png"
 
 export default function About({ navigation }) {
     return (
         <>
-            <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1, backgroundColor: "#fff" }}>
-                <StatusBar barStyle="light-content" backgroundColor="#044BD9"></StatusBar>
-                <View style={styles.container}>
-                    <Text>Sobre!</Text>
+            <StatusBar barStyle="light-content" backgroundColor="#044BD9"></StatusBar>
+            <View style={styles.container}>
+                <View style={styles.imageView}>
+                    <Image source={sobre} />
                 </View>
-            </ScrollView>
+                <View style={styles.textView}>
+                    <Text style={styles.textBold}>Pesquisador:</Text>
+                    <Text style={styles.textLight}>sdasddsasda</Text>
+                    <Text style={styles.textBold}>Orientação:</Text>
+                    <Text style={styles.textLight}>Prof. Dr. Francisco Adelton Alves Rrbeiro:</Text>
+                    <Text style={styles.textBold}>Coorientação:</Text>
+                    <Text style={styles.textLight}>Prof Dr. Alváro Itauna Schalcher Pereira</Text>
+                    <Text style={styles.textBold}>Desenvolvedores:</Text>
+                    <Text style={styles.textLight}>Pedro Lucas Souza Carneiro</Text>
+                    <Text style={styles.textLight}>Rainan Carneiro Araújo</Text>
+                    <Text style={styles.textBold}>Colaborador:</Text>
+                    <Text style={styles.textLight}>Kayo Emanuel Ribeiro de Araújo</Text>
+                </View>
+
+            </View>
+
         </>
     );
 }
@@ -20,36 +36,31 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: "#fafafa"
-    },
-    layout: {
-        alignSelf: "stretch",
-        paddingHorizontal: 10,
-        marginTop: 30,
-        flexDirection: "column"
+        backgroundColor: "#fff"
     },
     horizontalView: {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-around"
     },
-    buttonOption: {
-        backgroundColor: "#fff",
-        height: hp("20%"),
-        width: wp("43%"),
-        marginBottom: 20,
-        justifyContent: "center",
+    imageView: {
         alignItems: "center",
-        borderRadius: 10,
-        shadowColor: "#f5f5f5",
-        elevation: 1,
-        padding: hp("1%")
-
+        justifyContent: "center",
+        padding: 50
     },
-    textButtonOption: {
-        color: "#000",
+    textView: {
+        alignItems: "center",
+    },
+    textBold: {
         textAlign: "center",
-        fontSize: hp("2%"),
+        fontWeight: "bold",
+        fontSize: 14,
+        color: "#000"
+    },
+    textLight: {
+        textAlign: "center",
+        fontSize: 14,
+        color: "#000"
+    },
 
-    }
 })
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, StatusBar, ScrollView, FlatList, SafeAreaView } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, StatusBar, ScrollView, FlatList, SafeAreaView, Image } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import constitucional from "../../assets/constitucional.png"
+import direitoTrabalho from "../../assets/trabalho1.png"
+import estagio from "../../assets/trabalho.png"
+import consumidor from "../../assets/consumidor.png"
+import curiosidades from "../../assets/curiosidades.png"
+import aprendiz from "../../assets/aprendiz.png"
+import eca from "../../assets/eca.png"
+import familia from "../../assets/familia.png"
+import penal from "../../assets/penal.png"
+import previdencaiario from "../../assets/previdencario.png"
 
 const DATA = [
     {
-        name: "Direito Constitucional"
+        name: "Direito Constitucional",
+        image: constitucional
     },
     {
-        name: "Direito do Trabalho"
-    }
+        name: "Direito do Trabalho",
+        image: direitoTrabalho
+    },
+    {
+        name: "Direito do Consumidor",
+        image: consumidor
+    },
+    {
+        name: "Direito Penal",
+        image: penal
+    },
+    {
+        name: "Direito Previdenciário",
+        image: previdencaiario
+    },
+    {
+        name: "Direito da Família",
+        image: familia
+    },
+    {
+        name: "Lei de Estágio",
+        image: estagio
+    },
+    {
+        name: "Estatuto da Criança e do Adolescente - ECA",
+        image: eca
+    },
+    {
+        name: "Menor Aprendiz",
+        image: aprendiz
+    },
+    {
+        name: "Curiosidades",
+        image: curiosidades
+    },
+
 ]
 
 export default function Home({ navigation }) {
@@ -16,12 +61,17 @@ export default function Home({ navigation }) {
         <>
             <StatusBar barStyle="light-content" backgroundColor="#044BD9"></StatusBar>
             <FlatList
+                contentContainerStyle={{ paddingTop: 10 }}
                 numColumns={2}
                 data={DATA}
                 renderItem={({ item }) =>
-                    <View>
+                    <View style={styles.buttonOption}>
                         <TouchableOpacity
                             onPress={() => navigation.navigate("Home")}>
+                            <View style={styles.imageView}>
+                                <Image source={item.image} />
+                            </View>
+
                             <Text style={styles.textButtonOption}>
                                 {item.name}
                             </Text>
@@ -40,34 +90,31 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: "#fafafa"
     },
-    layout: {
-        alignSelf: "stretch",
-        paddingHorizontal: 10,
-        marginTop: 30,
-        flexDirection: "column"
-    },
-    horizontalView: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-around"
-    },
     buttonOption: {
+        width: wp("47%"),
+        height: wp("47%"),
         backgroundColor: "#fff",
-        height: hp("20%"),
-        width: wp("43%"),
-        marginBottom: 20,
+        marginBottom: wp("3%"),
         justifyContent: "center",
         alignItems: "center",
-        borderRadius: 10,
+        borderRadius: 15,
         shadowColor: "#f5f5f5",
         elevation: 1,
-        padding: hp("1%")
+        marginLeft: wp("2%"),
 
     },
     textButtonOption: {
         color: "#000",
         textAlign: "center",
         fontSize: hp("2%"),
+        marginBottom: wp("3%")
 
-    }
+    },
+    imageView: {
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        padding: 10
+    },
+
 })
