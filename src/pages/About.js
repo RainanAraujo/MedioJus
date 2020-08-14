d7d1bfd58d2561fda5677795aa2ecf3d5b81fefa
RainanAraujo <rainanaraaujo@gmail.com>
2020-08-14 20:56:42 -0300

    refractor scroView added in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,36 +17,40 @@ export default function About({navigation}) {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#0455BF"></StatusBar>
-      <View style={styles.container}>
-        <View style={styles.imageView}>
-          <Image source={sobre} style={styles.image} />
-          <Text style={styles.textAbout}>
-            Este aplicativo tem por objetivo possibilitar o contato dos
-            discentes de Ensino Médio com orientações ou conhecimentos
-            jurídicos, especialmente direitos e deveres, que contribuirão para a
-            formação, cada vez mais cedo, de cidadãos ativos, críticos, lúcidos
-            e conscientes de seu papel na construção de uma sociedade mais
-            livre, justa e solidária.
-          </Text>
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          <View style={styles.imageView}>
+            <Image source={sobre} style={styles.image} />
+            <Text style={styles.textAbout}>
+              Este aplicativo tem por objetivo possibilitar o contato dos
+              discentes de Ensino Médio com orientações ou conhecimentos
+              jurídicos, especialmente direitos e deveres, que contribuirão para
+              a formação, cada vez mais cedo, de cidadãos ativos, críticos,
+              lúcidos e conscientes de seu papel na construção de uma sociedade
+              mais livre, justa e solidária.
+            </Text>
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textBold}>Pesquisador:</Text>
+            <Text style={styles.textLight}>Lauro Henrique Borges Leal</Text>
+            <Text style={styles.textBold}>Orientação:</Text>
+            <Text style={styles.textLight}>
+              Prof. Dr. Francisco Adelton Alves Rrbeiro:
+            </Text>
+            <Text style={styles.textBold}>Coorientação:</Text>
+            <Text style={styles.textLight}>
+              Prof Dr. Alváro Itauna Schalcher Pereira
+            </Text>
+            <Text style={styles.textBold}>Desenvolvedores:</Text>
+            <Text style={styles.textLight}>Pedro Lucas Souza Carneiro</Text>
+            <Text style={styles.textLight}>Rainan Carneiro Araújo</Text>
+            <Text style={styles.textBold}>Colaborador:</Text>
+            <Text style={styles.textLight}>Kayo Emanuel Ribeiro de Araújo</Text>
+          </View>
         </View>
-        <View style={styles.textView}>
-          <Text style={styles.textBold}>Pesquisador:</Text>
-          <Text style={styles.textLight}>Lauro Henrique Borges Leal</Text>
-          <Text style={styles.textBold}>Orientação:</Text>
-          <Text style={styles.textLight}>
-            Prof. Dr. Francisco Adelton Alves Rrbeiro:
-          </Text>
-          <Text style={styles.textBold}>Coorientação:</Text>
-          <Text style={styles.textLight}>
-            Prof Dr. Alváro Itauna Schalcher Pereira
-          </Text>
-          <Text style={styles.textBold}>Desenvolvedores:</Text>
-          <Text style={styles.textLight}>Pedro Lucas Souza Carneiro</Text>
-          <Text style={styles.textLight}>Rainan Carneiro Araújo</Text>
-          <Text style={styles.textBold}>Colaborador:</Text>
-          <Text style={styles.textLight}>Kayo Emanuel Ribeiro de Araújo</Text>
-        </View>
-      </View>
+      </ScrollView>
     </>
   );
 }
@@ -71,6 +75,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 25,
+  },
+  scrollView: {
+    flexGrow: 1,
   },
   textView: {
     alignItems: 'center',
